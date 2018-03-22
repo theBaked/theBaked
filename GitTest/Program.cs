d1ec95e923843f363a408cae4a4f9447dc52ede1
Miguel <araujo.miguel.medeiros@gmail.com>
2018-03-22 11:06:47 -0300

    Primeiro Commit

---
+++ GitTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1, num2 = 2, result;
+            result = num + num2;
+
+            Console.WriteLine("resultado = {0}", result);
+
+
+            Console.ReadKey();
+        }
+    }
+}
